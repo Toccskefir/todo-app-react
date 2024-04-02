57dc72924334a5848348b85faadc7454f0cc7607
Tóth Kevin <tothkevin0716@gmail.com>
2024-04-02 14:37:39 +0200

    Added ToolBar component

---
+++ src/ToolBar.tsx
@@ -0,0 +1,19 @@
+import React, {useContext} from "react";
+import darkModeContext from "./DarkModeContext";
+
+function ToolBar() {
+    const {darkMode, setDarkMode} = useContext(darkModeContext);
+
+    return (
+        <div className={darkMode ? 'dark' : 'light'}>
+            <label>
+                {darkMode ? 'Change to Light Mode' : 'Change to Dark Mode'}
+                <input type="checkbox" checked={darkMode} onChange={(e) => {
+                    setDarkMode(e.target.checked);
+                }}/>
+            </label>
+        </div>
+    );
+}
+
+export default ToolBar;
